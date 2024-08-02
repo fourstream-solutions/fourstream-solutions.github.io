31e7ed252f55b229652f6fd4933e476a1c893e0d
fourstream-solutions <omkar@fourstream.in>
2024-08-02 17:23:23 +0530

    update

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@ document.addEventListener('DOMContentLoaded', function() {
     const navbarHTML = `
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
         <a class="navbar-brand" href="index.html">
-            <img src="assets/img/logo.png" alt="Logo" style="height: 40px;"/>
+            <img src="assets/imgs/logo.png" alt="Logo" style="height: 40px;"/>
         </a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
